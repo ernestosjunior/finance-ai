9921c89a072fce36b793a7477ab4a50feb1165db
Ernesto Junior <ernesto.sjunior@hotmail.coms>
2024-11-06 21:57:09 -0300

    chore: add clerk and config auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return <>Finance.ai</>;
-}
+const HomePage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) redirect("/login");
+
+  return (
+    <main className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </main>
+  );
+};
+
+export default HomePage;
